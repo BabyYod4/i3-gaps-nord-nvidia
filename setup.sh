0953a301f191f1960c5c529613476d94bb16679b
BabyYod4 <razzlenator@protonmail.com>
2022-08-19 16:22:28 +0200

    added vim compiled with +python3

--- setup.sh
+++ setup.sh
@@ -4,6 +4,9 @@ sudo pacman -Syyu
 sudo pacman -S base-devel
 sudo pacman -S yay
 
+sudo pacman -U downloads/vim/vim-9.0.0135-1-x86_64.pkg.tar.zst
+sudo pacman -U downloads/vim/vim-runtime-9.0.0135-1-x86_64.pkg.tar.zst
+
 yay -S polybar picom-ibhagwan-git ttf-ibm-plex font-awesome-5 ttf-unifont kitty rofi imagemagic texlive-core okular mailspring 
 
 xset -dpms
