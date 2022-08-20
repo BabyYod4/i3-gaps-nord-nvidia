618c60ce014e1ae606dc79f16ada0159ccda3707
BabyYod4 <razzlenator@protonmail.com>
2022-08-20 14:41:55 +0200

    Fixed vim binaries not uploading

--- setup.sh
+++ setup.sh
@@ -4,8 +4,12 @@ sudo pacman -Syyu
 sudo pacman -S base-devel
 sudo pacman -S yay
 
-sudo pacman -U downloads/vim/vim-9.0.0135-1-x86_64.pkg.tar.zst
-sudo pacman -U downloads/vim/vim-runtime-9.0.0135-1-x86_64.pkg.tar.zst
+yay -S zip unzip
+
+unzip downloads/vim/vim.zip -d downloads/vim
+
+sudo pacman -U downloads/vim/vim.pkg.tar.zst
+sudo pacman -U downloads/vim/vim-runtime.pkg.tar.zst
 
 yay -S polybar picom-ibhagwan-git ttf-ibm-plex font-awesome-5 ttf-unifont kitty rofi imagemagic texlive-core okular mailspring qt5-styleplugins 
 
