335a2c80db2e2390cfeb099d87d02a61def2c71c
BabyYod4 <razzlenator@protonmail.com>
2022-08-20 15:09:35 +0200

    attempt 3 to fix vim sources not uploading

--- setup.sh
+++ setup.sh
@@ -4,12 +4,10 @@ sudo pacman -Syyu
 sudo pacman -S base-devel
 sudo pacman -S yay
 
-yay -S zip unzip
-
-unzip downloads/vim/vim.zip -d downloads/vim
-
-sudo pacman -U downloads/vim/vim.pkg.tar.zst
-sudo pacman -U downloads/vim/vim-runtime.pkg.tar.zst
+cd downloads/vim
+makepkg -si --nocheck
+sudo pacman -U *.pkg.tar.zst
+cd ~
 
 yay -S polybar picom-ibhagwan-git ttf-ibm-plex font-awesome-5 ttf-unifont kitty rofi imagemagic texlive-core okular mailspring qt5-styleplugins 
 
