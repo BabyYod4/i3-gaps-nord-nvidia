accd0b8925adbd8374faff46600f702d874c5131
BabyYod4 <razzlenator@protonmail.com>
2022-08-20 14:27:22 +0200

    made sure QT5 uses GTK+ as default theme to ensure QT5 themes also use Nord theme (ex Okular)

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ sudo pacman -S yay
 sudo pacman -U downloads/vim/vim-9.0.0135-1-x86_64.pkg.tar.zst
 sudo pacman -U downloads/vim/vim-runtime-9.0.0135-1-x86_64.pkg.tar.zst
 
-yay -S polybar picom-ibhagwan-git ttf-ibm-plex font-awesome-5 ttf-unifont kitty rofi imagemagic texlive-core okular mailspring 
+yay -S polybar picom-ibhagwan-git ttf-ibm-plex font-awesome-5 ttf-unifont kitty rofi imagemagic texlive-core okular mailspring qt5-styleplugins 
 
 xset -dpms
 xset s noblank
